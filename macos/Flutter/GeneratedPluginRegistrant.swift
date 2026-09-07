03c7332dea1b036b86ec05cef0b3c910a2c7aa03
韶飞轶事 <junrongzou@qq.com>
2026-09-08 00:22:12 +0800

    chore: 钉住 workmanager 0.9.0+3 版本族，绕开 iOS 26 API 与 Xcode 16.2 不兼容

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -29,7 +29,6 @@ import sqflite_darwin
 import url_launcher_macos
 import wakelock_plus
 import window_manager
-import workmanager_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
@@ -56,5 +55,4 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
-  WorkmanagerPlugin.register(with: registry.registrar(forPlugin: "WorkmanagerPlugin"))
 }
